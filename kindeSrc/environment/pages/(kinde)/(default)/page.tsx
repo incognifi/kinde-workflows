aa9a9cd2475831b9ba45152e4ca8f33f083c3563
James Woodley <james@lightestnight.com>
2025-08-11 13:14:28 +0100

    Total strippage

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -1,20 +1,40 @@
 "use server";
 
-import { Widget } from "../../../../components/widget";
-import { DefaultLayout } from "../../../../layouts/default";
-import { type KindePageEvent } from "@kinde/infrastructure";
 import React from "react";
 import { renderToString } from "react-dom/server.browser";
-import { Root } from "../../../../root";
+
+import {
+  getKindeCSRF,
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  getSVGFaviconUrl,
+  type KindePageEvent,
+} from "@kinde/infrastructure";
 
 const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
   return (
-    <Root context={context} request={request}>
-      <div>Test Content</div>
-      {/* <DefaultLayout>
-        <Widget heading="Login to Incognifi" />
-      </DefaultLayout> */}
-    </Root>
+    <html dir={request.locale.isRtl ? "rtl" : "ltr"} lang={request.locale.lang}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta content="noindex" name="robots" />
+        <meta content={getKindeCSRF()} name="csrf-token" />
+        <meta content="light" name="color-scheme" />
+        <meta content="nopagereadaloud" name="google" />
+        <meta content="Login to your Incognifi account" name="description" />
+
+        <title>Login - Incognifi</title>
+
+        <link href={getSVGFaviconUrl()} rel="icon" type="image/svg+xml" />
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
+      </head>
+
+      <body>
+        <div data-kinde-root="true">INCOGNIFI TEST</div>
+      </body>
+    </html>
   );
 };
 
